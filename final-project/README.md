7d754c7bef1e7ad10c7b150c557195271275b1fb
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2021-10-16 04:25:20 -0400

    Added final project files

---
+++ final-project/README.md
@@ -0,0 +1,2 @@
+# final-project
+# Web 4 Link: https://in-info-web4.informatics.iupui.edu/~jtadrous/final-project/
---
+++ final-project/model/model.js
@@ -0,0 +1,294 @@
+var homeContent = `<section class="home">
+<div class="quote">
+    <p class="text">“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo”
+    </p>
+    <p class="name">John Smith</p>
+    <p class="job">Corporation CEO, books author.</p>
+</div>
+<div class="events">
+  <div class="contain">
+    <p class="head">UPCOMING EVENTS:</p>
+    <div class="calendar">
+        <div class="date">
+            <div class="pink-box">
+                <p class="num">06</p>
+                <p class="mo">JUN</p>
+            </div>
+            <div class="right">
+                <p class="top">Sed et persipiatis unde omnis iste natus</p>
+                <p class="bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+            </div>
+        </div>
+        <div class="date">
+            <div class="pink-box">
+                <p class="num">30</p>
+                <p class="mo">JUL</p>
+            </div>
+            <div class="right">
+                <p class="top">Sed et persipiatis unde omnis iste natus</p>
+                <p class="bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+            </div>
+        </div>
+        <div class="date">
+            <div class="pink-box">
+                <p class="num">30</p>
+                <p class="mo">AUG</p>
+            </div>
+            <div class="right">
+                <p class="top">Sed et persipiatis unde omnis iste natus</p>
+                <p class="bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+            </div>
+        </div>
+        <div class="date">
+            <div class="pink-box">
+                <p class="num">23</p>
+                <p class="mo">NOV</p>
+            </div>
+            <div class="right">
+                <p class="top">Sed et persipiatis unde omnis iste natus</p>
+                <p class="bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+            </div>
+        </div>
+        <div class="date">
+            <div class="pink-box">
+                <p class="num">23</p>
+                <p class="mo">DEC</p>
+            </div>
+            <div class="right">
+                <p class="top">Sed et persipiatis unde omnis iste natus</p>
+                <p class="bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+</section>`;
+
+var aboutContent = `<section class="about">
+<div class="head">
+  <p class="title">OUR HISTORY:</p>
+</div>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</section>`;
+
+var galleryContent = `<section class="gallery">
+<p>GALLERY:</p>
+<div class="collage">
+    <div id="pic1"></div>
+    <div id="pic2"></div>
+    <div id="pic3"></div>
+</div>
+<div class="collage">
+    <div id="pic4"></div>
+    <div id="pic5"></div>
+</div>
+</section>`;
+
+var pic1Content = `<section class="galpage">
+<div class="top food">
+    <p class="num">1.</p>
+    <p class="title">food festival</p>
+</div>
+<div class="text">
+    <p class="left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p class="right">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</div>
+</section>`;
+
+var pic2Content = `<section class="galpage">
+<div class="top dj">
+    <p class="num">2.</p>
+    <p class="title">dee-jay</p>
+</div>
+<div class="text">
+    <p class="left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p class="right">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</div>
+</section>`;
+
+var pic3Content = `<section class="galpage">
+<div class="top speech">
+    <p class="num">3.</p>
+    <p class="title">speech</p>
+</div>
+<div class="text">
+    <p class="left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p class="right">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</div>
+</section>`;
+
+var pic4Content = `<section class="galpage">
+<div class="top open">
+    <p class="num">4.</p>
+    <p class="title">open foodfest</p>
+</div>
+<div class="text">
+    <p class="left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p class="right">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</div>
+</section>`;
+
+var pic5Content = `<section class="galpage">
+<div class="top inter">
+    <p class="num">5.</p>
+    <p class="title">international</p>
+</div>
+<div class="text">
+    <p class="left">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p class="right">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</div>
+</section>`;
+
+var blogContent = `<section class="blog">
+<p class="head">BLOG:</p>
+<div class="dates">
+    <div class="date" id="jun06">
+        <div class="pink-box">
+            <p class="num">06</p>
+            <p class="mo">JUN</p>
+        </div>
+        <div class="right">
+            <div class="banner"></div>
+            <p class="title">Sed et persipiatis unde omnis iste natus</p>
+            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+    </div>
+    <div class="date" id="jul30">
+        <div class="pink-box">
+            <p class="num">30</p>
+            <p class="mo">JUL</p>
+        </div>
+        <div class="right">
+            <div class="banner"></div>
+            <p class="title">Sed et persipiatis unde omnis iste natus</p>
+            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+    </div>
+    <div class="date" id="aug30">
+        <div class="pink-box">
+            <p class="num">30</p>
+            <p class="mo">AUG</p>
+        </div>
+        <div class="right">
+            <div class="banner"></div>
+            <p class="title">Sed et persipiatis unde omnis iste natus</p>
+            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+    </div>
+    <div class="date" id="nov23">
+        <div class="pink-box">
+            <p class="num">23</p>
+            <p class="mo">NOV</p>
+        </div>
+        <div class="right">
+            <div class="banner"></div>
+            <p class="title">Sed et persipiatis unde omnis iste natus</p>
+            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+    </div>
+    <div class="date" id="dec23">
+        <div class="pink-box">
+            <p class="num">23</p>
+            <p class="mo">DEC</p>
+        </div>
+        <div class="right">
+            <div class="banner"></div>
+            <p class="title">Sed et persipiatis unde omnis iste natus</p>
+            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        </div>
+    </div>
+</div>
+</section>`;
+
+var jun06Content = `<section class="post">
+<div class="banner">
+    <div class="pink-box">
+        <p class="num">06</p>
+        <p class="mo">JUN</p>
+    </div>
+</div>
+<p class="head">Header #1:</p>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p class="head">Header #2:</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</section>`;
+
+var jul30Content = `<section class="post">
+<div class="banner">
+    <div class="pink-box">
+        <p class="num">30</p>
+        <p class="mo">JUL</p>
+    </div>
+</div>
+<p class="head">Header #1:</p>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p class="head">Header #2:</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</section>`;
+
+var aug30Content = `<section class="post">
+<div class="banner">
+    <div class="pink-box">
+        <p class="num">30</p>
+        <p class="mo">AUG</p>
+    </div>
+</div>
+<p class="head">Header #1:</p>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p class="head">Header #2:</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</section>`;
+
+var nov23Content = `<section class="post">
+<div class="banner">
+    <div class="pink-box">
+        <p class="num">23</p>
+        <p class="mo">NOV</p>
+    </div>
+</div>
+<p class="head">Header #1:</p>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p class="head">Header #2:</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</section>`;
+
+var dec23Content = `<section class="post">
+<div class="banner">
+    <div class="pink-box">
+        <p class="num">23</p>
+        <p class="mo">DEC</p>
+    </div>
+</div>
+<p class="head">Header #1:</p>
+<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+<p class="head">Header #2:</p>
+<p class="para">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+</section>`;
+
+var contactContent = `<section class="contact">
+<p class="head">CONTACT US:</p>
+<div class="contain">
+    <div class="left">
+        <form action="#">
+            <input type="text" id="name" name="name" placeholder="Your name...">
+            <input type="email" id="email" name="email" placeholder="Email Address...">
+            <input type="text" id="company" name="company" placeholder="Company...">
+            <textarea name="message" id="message" cols="30" rows="9" placeholder="Message..."></textarea>
+            <input type="submit" value="SEND MESSAGE" id="submit">
+        </form>
+    </div>
+    <div class="right"></div>
+</div>
+</section>`;
+
+export function getPageContent(pageId) {
+    //getting the element with the id="app"
+    //changes the text in that div to the button's id that was clicked
+    //this changes the string in the contentId into reading it as variable
+    $("#app").html(eval(pageId));
+};
